07936d87630206296a236260b6c5c42d49a435db
tbright32 <jbright@intergate.com>
2018-06-26 21:56:08 -0400

    JUST A CODE CLEANING

--- app/src/main/java/com/example/musicmentor/musicmentor/MainActivity.java
+++ app/src/main/java/com/example/musicmentor/musicmentor/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     TextView task_1;
     TextView video_1;
     Button button;
-    VideoView videoView;
     VideoView resultVideo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,43 +29,53 @@ public class MainActivity extends AppCompatActivity {
 
         video_1 = (TextView) findViewById(R.id.video_1);
 
-        task_1.setVisibility(View.GONE);    // hide until lesson is clicked
-        video_1.setVisibility(View.GONE);   // hide until task is clicked
-
         button = (Button) findViewById(R.id.button);
         resultVideo = (VideoView) findViewById(R.id.videoView);
+
+        // Hide until menu "parents" are clicked
+        task_1.setVisibility(View.GONE);
+        video_1.setVisibility(View.GONE);
+        button.setVisibility(View.GONE);
+        resultVideo.setVisibility(View.GONE);
     }
+
     /**
-     * onClick handler
+     * begin onClick handlers
      */
     public void toggle_lesson(View v) {
         task_1.setVisibility( task_1.isShown()
                 ? View.GONE
                 : View.VISIBLE );
         video_1.setVisibility( View.GONE );
-//        resultVideo.setVisibility( View.GONE );
-//        button.setVisibility( View.GONE );
+        resultVideo.setVisibility( View.GONE );
+        button.setVisibility( View.GONE );
     }
 
     public void toggle_task(View v) {
         video_1.setVisibility( video_1.isShown()
                 ? View.GONE
                 : View.VISIBLE );
-//        resultVideo.setVisibility( View.GONE );
-//        button.setVisibility( View.GONE );
+        resultVideo.setVisibility( View.GONE );
+        button.setVisibility( View.GONE );
     }
 
-//    public void toggle_video(View v) {
-//        resultVideo.setVisibility( resultVideo.isShown()
-//            ? View.GONE
-//            : View.VISIBLE);
-//
-//        button.setVisibility( resultVideo.isShown()
-//            ? View.GONE
-//            : View.VISIBLE);
-//    }
+    public void toggle_video(View v) {
+        resultVideo.setVisibility( resultVideo.isShown()
+            ? View.GONE
+            : View.VISIBLE);
 
+        button.setVisibility( button.isShown()
+            ? View.GONE
+            : View.VISIBLE);
+    }
 
+    /**
+     * End onClick handlers (sorta)
+     */
+
+    /**
+     * Record video
+     */
     public void dispatchTakeVideoIntent(View videoView) {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
@@ -74,7 +83,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    /**
+     * Handle resultant video
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
