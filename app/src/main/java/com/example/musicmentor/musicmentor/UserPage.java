679a27c83d0b13ef65cc9e6db42d6914cf78cce2
kirill_korygin <kk14665@gatech.edu>
2018-07-19 11:54:44 -0400

    user page logout

--- app/src/main/java/com/example/musicmentor/musicmentor/UserPage.java
+++ app/src/main/java/com/example/musicmentor/musicmentor/UserPage.java
@@ -1,13 +1,36 @@
 package com.example.musicmentor.musicmentor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+
+
+        Button logout = (Button) findViewById(R.id.UPLog);
+        logout.setOnClickListener (new View.OnClickListener() {
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
