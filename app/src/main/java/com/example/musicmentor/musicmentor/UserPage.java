74dcd162e0ac303eb0867558e2640f4a204d1fac
kirill_korygin <kk14665@gatech.edu>
2018-07-19 11:46:44 -0400

    user page framework

---
+++ app/src/main/java/com/example/musicmentor/musicmentor/UserPage.java
@@ -0,0 +1,13 @@
+package com.example.musicmentor.musicmentor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+    }
+}
